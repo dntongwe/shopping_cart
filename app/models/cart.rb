fb116f18ff0601b2701d402f617d031e90ee9d79
“dntongwe” <dmn55@cornell.edu>
2020-01-01 19:13:56 -0500

    paypal integration

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,4 +5,30 @@ class Cart < ApplicationRecord
 	def total_price
 		cart_items.to_a.sum(&:full_price)
 	end
+
+
+
+def paypal_url(return_url)
+  values = {
+    :business => 'sb-n9vvk38491@business.example.com',
+    :cmd => '_cart',
+    :upload => 1,
+    :return => return_url,
+    :invoice => id
+  }
+  cart_items.each_with_index do |item, index|
+    values.merge!({
+      "amount_#{index+1}" => item.unit_price,
+      "item_name_#{index+1}" => item.product.name,
+      "item_number_#{index+1}" => item.id,
+      "quantity_#{index+1}" => item.quantity
+    })
+  end
+  "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+end
+
+
+
+
+
 end
