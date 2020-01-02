a858ef6e8c65d6a7a9dcd24d7f11cdb244f8e776
“dntongwe” <dmn55@cornell.edu>
2020-01-02 00:41:45 -0500

    Paypal with Encryption

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,28 +8,37 @@ class Cart < ApplicationRecord
 
 
 
-def paypal_url(return_url, notify_url)
-  values = {
-    :business => 'sb-n9vvk38491@business.example.com',
-    :cmd => '_cart',
-    :upload => 1,
-    :return => return_url,
-    :invoice => id,
-    :notify_url => notify_url
-  }
-  cart_items.each_with_index do |item, index|
-    values.merge!({
-      "amount_#{index+1}" => item.unit_price,
-      "item_name_#{index+1}" => item.product.name,
-      "item_number_#{index+1}" => item.id,
-      "quantity_#{index+1}" => item.quantity
-    })
+
+  def paypal_encrypted(return_url, notify_url)
+    values = {
+      :business => APP_CONFIG[:paypal_email],
+      :cmd => '_cart',
+      #:cmd => "_s-xclick",
+      :upload => 1,
+      :return => return_url,
+      :invoice => id,
+      :notify_url => notify_url,
+      :cert_id =>'E5FAYYTHZYYGA'
+      #:cert_id => APP_CONFIG[:paypal_cert_id]
+    }
+    cart_items.each_with_index do |item, index|
+      values.merge!({
+        "amount_#{index+1}" => item.unit_price,
+        "item_name_#{index+1}" => item.product.name,
+        "item_number_#{index+1}" => item.id,
+        "quantity_#{index+1}" => item.quantity
+      })
+    end
+    encrypt_for_paypal(values)
   end
-  "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
-end
 
+  PAYPAL_CERT_PEM = File.read("#{Rails.root}/certs/paypal_cert.pem")
+  APP_CERT_PEM = File.read("#{Rails.root}/certs/app_cert.pem")
+  APP_KEY_PEM = File.read("#{Rails.root}/certs/app_key.pem")
 
-
-
+  def encrypt_for_paypal(values)
+    signed = OpenSSL::PKCS7::sign(OpenSSL::X509::Certificate.new(APP_CERT_PEM), OpenSSL::PKey::RSA.new(APP_KEY_PEM, ''), values.map { |k, v| "#{k}=#{v}" }.join("\n"), [], OpenSSL::PKCS7::BINARY)
+    OpenSSL::PKCS7::encrypt([OpenSSL::X509::Certificate.new(PAYPAL_CERT_PEM)], signed.to_der, OpenSSL::Cipher::Cipher::new("DES3"), OpenSSL::PKCS7::BINARY).to_s.gsub("\n", "")
+  end
 
 end
